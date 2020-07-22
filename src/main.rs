37f2c03fadbc9b98692c4835decbd7734d92dada
Curtis Jones <ikubetoomuzik@gmail.com>
2020-07-22 00:04:04 -0400

    reorganized info file structure

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use chat_server::*;
 
 fn main() -> Result<(), &'static str> {
     let mut app = App::new();
-    app.load_users("users")?;
-    app.load_convs("convs")?;
-    app.load_msgs("msgs")?;
-    app.load_rels("rels")?;
+    app.load_users("files/users")?;
+    app.load_convs("files/convs")?;
+    app.load_msgs("files/msgs")?;
+    app.load_rels("files/rels")?;
 
     // let me = app.get_user(Some("Curtis Jones"), None).unwrap();
     // let conv = app.get_conv(Some("Basic"), None).unwrap();
@@ -15,14 +15,14 @@ fn main() -> Result<(), &'static str> {
     // let users = app.get_user_mult(Some("s "), Some("mail"));
     // let convs = app.get_conv_mult(Some("bas"), None);
 
-    let curtis = app.get_user("NAME","Curtis Jones").unwrap();
+    let curtis = app.get_user("NAME", "Curtis Jones").unwrap();
     println!("{:#?}", curtis);
-    let sarah = app.get_user("NAME","Sarah Parsons").unwrap();
+    let sarah = app.get_user("NAME", "Sarah Parsons").unwrap();
 
     println!("{:#?}", app.get_rel_status(curtis, sarah));
 
     TcpServer::listen(8080, &mut app);
     // app.add_user("Curtis Jones", "mail@curtisjones.ca")?;
 
-    app.close("msgs", "convs", "users", "rels")
+    app.close("files/msgs", "files/convs", "files/users", "files/rels")
 }
